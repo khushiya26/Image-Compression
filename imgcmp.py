55e5edc2ad5fc2f5a64a05a1eec7c7c50db0ccc1
khushiya26 <143216881+khushiya26@users.noreply.github.com>
2024-09-07 17:56:41 +0530

    Update imgcmp.py

--- imgcmp.py
+++ imgcmp.py
@@ -2,8 +2,8 @@ from PIL import Image
 import os
 def compress_image(input_path, output_path, quality=85):
     try:
-        img = Image.open("C:\\Users\\Nandu\\Desktop\\Nandana-p\\Images\\car.jpeg")
-        img.save("C:\\Users\\Nandu\\Desktop\\Nandana-p\Images\\compressed image.jpeg", quality=quality)
+        img = Image.open("C:\\Users\\Khushi\\Desktop\\Pictures\\Images\\car.jpeg")
+        img.save("C:\\Users\\Khushi\\Desktop\\Pictures\Images\\compressed image.jpeg", quality=quality)
         print(f"Image compressed successfully and saved to {output_path}")
     except Exception as e:
         print(f"Error: {e}")
